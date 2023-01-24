df865563801efd0704742a4c021b1cb65a1ec5b1
Katya <katya6040@gmail.com>
2023-01-24 23:03:37 +0300

    Initial commit

---
+++ tasks/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("введите трехзначное число");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+while(Number >= 100)
+{
+    Number = Number / 10; 
+}
+
+int result = Number % 10;
+
+Console.WriteLine(result);
