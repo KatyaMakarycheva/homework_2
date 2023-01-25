fcd660f536d13eccc82996773e9ab51f6f11d9a6
Katya <katya6040@gmail.com>
2023-01-25 21:19:43 +0300

    Task3

--- tasks/task_3/Program.cs
+++ tasks/task_3/Program.cs
@@ -2,20 +2,14 @@
 
 Console.WriteLine("Введите номер дня недели: ");
 
-int[] array = {1, 2, 3, 4, 5, 6, 7};
-int d = array.Length:
-int day = Convert.ToInt32(Console.ReadLine());
+int[] array = {1, 2, 3, 4, 5, 6, 7}; 
+int d = Convert.ToInt32(Console.ReadLine());
 
-if(d >= 6)
-    { 
-        Console.WriteLine("Это выходной день");
-    }
- else if(d < 6)
- {
-    Console.WriteLine("Это рабочий день");
- }
-
- else(d > 7)
- {
-    Console.WriteLine("Такого дня недели нет");
- }
+if (d == 6 || d == 7)
+{
+  Console.WriteLine("Это выходной");
+}
+else 
+{
+   Console.WriteLine("Это рабочий день");
+}
